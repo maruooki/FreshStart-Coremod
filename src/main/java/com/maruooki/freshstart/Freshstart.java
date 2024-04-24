29dbd4a2338e35af14f27b5104252a7e4dabbe2c
maru <maruooky@gmail.com>
2024-04-24 01:37:42 -0400

    Added first chest loot table

--- src/main/java/com/maruooki/freshstart/Freshstart.java
+++ src/main/java/com/maruooki/freshstart/Freshstart.java
@@ -1,5 +1,8 @@
 package com.maruooki.freshstart;
 
+import com.maruooki.freshstart.World.CustomChests.LootGame1;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -9,12 +12,21 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 public class Freshstart
 {
     public static final String MODID = "freshstart";
-    public static final String VERSION = "@VERSION@";
+    public static final String VERSION = "1.0.0";
+
+    @Mod.Instance("freshstart")
+    public static Freshstart instance;
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    public void preinit(FMLPreInitializationEvent event) {
+        LootGame1.initChestGenHook();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+}
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
     }
 }
