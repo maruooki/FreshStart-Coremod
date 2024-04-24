cea774a53d648bbc2bcb652d179ae8a15c1f4947
maru <maruooky@gmail.com>
2024-04-24 02:18:50 -0400

    Added other lootGame tables

--- src/main/java/com/maruooki/freshstart/Freshstart.java
+++ src/main/java/com/maruooki/freshstart/Freshstart.java
@@ -1,25 +1,35 @@
 package com.maruooki.freshstart;
 
 import com.maruooki.freshstart.World.CustomChests.LootGame1;
+import com.maruooki.freshstart.World.CustomChests.LootGame2;
+import com.maruooki.freshstart.World.CustomChests.LootGame3;
+import com.maruooki.freshstart.World.CustomChests.LootGame4;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = Freshstart.MODID, version = Freshstart.VERSION)
+@Mod(
+        modid = Freshstart.MODID,
+        version = Freshstart.VERSION
+)
 public class Freshstart
 {
     public static final String MODID = "freshstart";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     @Mod.Instance("freshstart")
     public static Freshstart instance;
 
     @EventHandler
     public void preinit(FMLPreInitializationEvent event) {
+        // currently, these are configured through TooMuchLoot, they will have embedded tables eventually.
         LootGame1.initChestGenHook();
+        LootGame2.initChestGenHook();
+        LootGame3.initChestGenHook();
+        LootGame4.initChestGenHook();
+
     }
 
     @EventHandler
---
+++ src/main/java/com/maruooki/freshstart/World/CustomChests/LootGame3.java
@@ -0,0 +1,19 @@
+package com.maruooki.freshstart.World.CustomChests;
+
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.WeightedRandomChestContent;
+import net.minecraftforge.common.ChestGenHooks;
+
+public class LootGame3 {
+    public LootGame3() {
+    }
+    public static void initChestGenHook() {
+        String key = "freshstart.LOOT_GAME3";
+        ChestGenHooks info = ChestGenHooks.getInfo(key);
+        info.setMin(1);
+        info.setMax(2);
+        info.addItem(new WeightedRandomChestContent(new ItemStack(Items.iron_ingot, 1, 0), 1, 4, 40));
+        info.addItem(new WeightedRandomChestContent(new ItemStack(Items.netherbrick, 1, 0), 1,4, 90));
+    }
+}
